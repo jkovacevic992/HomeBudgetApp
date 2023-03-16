0a022e767d420c37f9b1f3d527f4eebcda7ae9e8
josip <jkovacevic992@gmail.com>
2023-03-16 13:28:57 +0100

    Changed password hashing

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -7,6 +7,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -14,10 +15,12 @@ class RegisterController extends AbstractController
 
     /**
      * @param ManagerRegistry $managerRegistry
+     * @param UserPasswordHasherInterface $passwordHasher
      */
-    public function __construct(public ManagerRegistry $managerRegistry)
-    {
-    }
+    public function __construct(
+        public ManagerRegistry $managerRegistry,
+        public UserPasswordHasherInterface $passwordHasher
+    ){}
 
     /**
      * Method used to register the user using data from request
@@ -31,7 +34,11 @@ class RegisterController extends AbstractController
 
         $user = new User();
         $user->setEmail($data['email']);
-        $user->setPassword(password_hash($data['password'], algo: PASSWORD_DEFAULT));
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            user: $user,
+            plainPassword: $data['password']
+        );
+        $user->setPassword($hashedPassword);
 
         $entityManager = $this->managerRegistry->getManager();
         $entityManager->persist($user);

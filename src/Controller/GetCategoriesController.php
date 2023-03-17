278862e8983e9e042d9cf46e4b7c3d4e3ff9a16e
josip <jkovacevic992@gmail.com>
2023-03-17 12:35:27 +0100

    GET categories

---
+++ src/Controller/GetCategoriesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GetCategoriesController extends AbstractController
+{
+    public function __construct(private CategoryRepositoryInterface $categoryRepository)
+    {
+    }
+
+    #[Route('/api/categories', name: 'app_get_categories', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $data = $this->categoryRepository->findAll();
+        return $this->json($data);
+    }
+}
